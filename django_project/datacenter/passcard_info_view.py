14228c0833bdfa181eb928f660130acc62e5e9ab
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-02 17:46:55 +0300

    Update passcard_info_view.py
    Код исправлен по форматам PEP8

--- django_project/datacenter/passcard_info_view.py
+++ django_project/datacenter/passcard_info_view.py
@@ -7,16 +7,19 @@ from functions import get_duration, format_duration, is_visit_long
 
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
-    this_passcard_visits = []    
+    this_passcard_visits = []
     for visit in Visit.objects.filter(passcard=passcard):
         hours, minutes, seconds = get_duration(visit)
         this_passcard_visits.append({'entered_at': visit.entered_at,
-                                     'duration': format_duration(hours, minutes, seconds),
-                                     'is_strange': is_visit_long(visit, 25)           
+                                     'duration': format_duration(hours,
+                                                                 minutes,
+                                                                 seconds),
+                                     'is_strange': is_visit_long(visit, 25)
                                      }
-                                    )    
+                                    )
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
+
