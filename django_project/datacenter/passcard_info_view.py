dd30fa02cc0433def695a81aac9891ede84b1b2b
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-02 18:18:57 +0300

    Update passcard_info_view.py
    переменная TZ добавляется из файла настроек

--- django_project/datacenter/passcard_info_view.py
+++ django_project/datacenter/passcard_info_view.py
@@ -3,13 +3,16 @@ from django_project.datacenter.models import Visit
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from functions import get_duration, format_duration, is_visit_long
+from django.conf import settings
+
 visit_threshold = 25 # максимальная длительность визита установлена 25 минут
+TZ=settings.TZ
 
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
     this_passcard_visits = []
     for visit in Visit.objects.filter(passcard=passcard):
-        hours, minutes, seconds = get_duration(visit)
+        hours, minutes, seconds = get_duration(visit, TZ)
         this_passcard_visits.append({'entered_at': visit.entered_at,
                                      'duration': format_duration(hours,
                                                                  minutes,
