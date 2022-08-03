d15649857a17e969d87eb370da2772764857cb64
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-03 21:57:49 +0300

    Update functions.py
    TZ-лишний, он итак подгружается из настроек

--- functions.py
+++ functions.py
@@ -1,13 +1,12 @@
 from django.utils.timezone import localtime
 import pytz
 
+MAX_VISIT_DURATION = 25
 
-def get_duration(visit, TZ):
-    entered_time = localtime(visit.entered_at, pytz.timezone(TZ))
-    if not visit.leaved_at:
-        left_time = current_time
-    else:
-        left_time = localtime(visit.leaved_at, pytz.timezone(TZ))
+
+def get_duration(visit):
+    entered_time = localtime(visit.entered_at)
+    left_time = localtime(visit.leaved_at)
     time_in_storage = left_time - entered_time
     seconds = time_in_storage.total_seconds()
     hours = round(seconds // 3600)
@@ -23,5 +22,5 @@ def format_duration(hours, minutes, seconds):
 
 def is_visit_long(visit, threshhold):
     hours, minutes, seconds = get_duration(visit)
-    return hours * 60 + minutes > threshhold 
+    return hours * 60 + minutes > threshhold
 
