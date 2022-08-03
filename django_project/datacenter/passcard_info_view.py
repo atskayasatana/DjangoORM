358984008caabc4f33b5f4722b8625b1cefff160
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-08-03 22:01:05 +0300

    Update passcard_info_view.py

--- django_project/datacenter/passcard_info_view.py
+++ django_project/datacenter/passcard_info_view.py
@@ -2,28 +2,24 @@ from django_project.datacenter.models import Passcard
 from django_project.datacenter.models import Visit
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from functions import get_duration, format_duration, is_visit_long
+from functions import get_duration, format_duration,\
+                      is_visit_long, MAX_VISIT_DURATION
 from django.conf import settings
-
-
-visit_threshold = 25   # максимальная длительность визита установлена 25 минут
-
-
-TZ = settings.TIME_ZONE
 
 
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
     this_passcard_visits = []
     for visit in Visit.objects.filter(passcard=passcard):
-        hours, minutes, seconds = get_duration(visit, TZ)
-        this_passcard_visits.append({'entered_at': visit.entered_at,
+        hours, minutes, seconds = get_duration(visit)
+        this_passcard_visits.append({
+                                     'entered_at': visit.entered_at,
                                      'duration': format_duration(hours,
                                                                  minutes,
                                                                  seconds),
                                      'is_strange': is_visit_long(
                                                                 visit, 
-                                                                visit_threshold
+                                                                MAX_VISIT_DURATION
                                                                 )
                                      }
                                     )
@@ -32,4 +28,5 @@ def passcard_info_view(request, passcode):
         'this_passcard_visits': this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
+
 
